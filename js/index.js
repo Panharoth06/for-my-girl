4e8936c7e13a79510e8e591917083a2e10653ec4
Panharoth06 <panharothchheng0903@gmail.com>
2025-04-07 00:08:11 +0700

    for-her-2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Have Something for You🩷'.split('')
+const text = 'Have Something for You'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
